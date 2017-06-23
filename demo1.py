bba0a7b1ff05c81542a32e9ce555e8113565a0a9
lmyundo@gmail.com <YSYSWAN++>
2017-06-23 21:01:51 +0800

    ok

--- demo1.py
+++ demo1.py
@@ -13,5 +13,5 @@ url="http://www.baidu.com/s?"
 full_url=url+url_values
  
 data=urllib.request.urlopen(full_url).read()
-data=data.decode('gb2312','utf-8')
+data=data.decode('utf-8','ignore')
 print(data)
