07e024dff24bde8bc790c797c63e6e46b9021f26
lmyundo@gmail.com <YSYSWAN++>
2017-06-25 00:38:40 +0800

    调试
    调试

--- demo4.py
+++ demo4.py
@@ -3,17 +3,17 @@ from email.mime.text import MIMEText
 from email.header import Header
  
 # 第三方 SMTP 服务
-emailcontainer='dfghjkl'
-mail_host="smtp.mxhichina.com"  #设置服务器
-mail_user="jomain@changewaver.com"    #用户名
-mail_pass="Lmy199864"   #口令 
+emailcontainer='dfghjklksldfjlksdjflsdjflsadjflsjd sdkfjlds kjlsadjfkls lksdajfl'
+mail_host="smtp.qq.com"  #设置服务器
+mail_user="jomain@ahusec.cn"    #用户名
+mail_pass="xtscwxhdnkltjejd"   #口令 
  
  
-sender = 'jomain@changewaver.com'
+sender = 'jomain@ahusec.cn'
 receivers = ['1075823894@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
  
-message = MIMEText('ftyft', 'plain', 'utf-8')
-message['From'] = Header("jomain@changewaver.com", 'utf-8')
+message = MIMEText('emailcontainer', 'plain', 'utf-8')
+message['From'] = Header("jomain@ahusec.cn", 'utf-8')
 message['To'] =  Header("1075823894@qq.com", 'utf-8')
  
 subject = 'new'
@@ -22,7 +22,7 @@ message['Subject'] = Header(subject, 'utf-8')
  
 try:
     smtpObj = smtplib.SMTP() 
-    smtpObj.connect(mail_host, 25)    # 25 为 SMTP 端口号
+    smtpObj.connect(mail_host, 587)    # 25 为 SMTP 端口号
     smtpObj.login(mail_user,mail_pass)
     smtpObj.sendmail(sender, receivers, message.as_string())
     print ("邮件发送成功")
