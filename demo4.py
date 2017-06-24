046247968cb88e2dc158f77cf72be5a235e24d54
lmyundo@gmail.com <YSYSWAN++>
2017-06-25 00:55:05 +0800

    fuck
    fuck

--- demo4.py
+++ demo4.py
@@ -3,17 +3,17 @@ from email.mime.text import MIMEText
 from email.header import Header
  
 # 第三方 SMTP 服务
-emailcontainer='dfghjklksldfjlksdjflsdjflsadjflsjd sdkfjlds kjlsadjfkls lksdajfl'
+# emailcontainer='dfghjklksldfjlksdjflsdjflsadjflsjd sdkfjlds kjlsadjfkls lksdajfl'
 mail_host="smtp.qq.com"  #设置服务器
-mail_user="jomain@ahusec.cn"    #用户名
-mail_pass="xtscwxhdnkltjejd"   #口令 
+mail_user="1273046149@qq.com"    #用户名
+mail_pass="ofkaypupwehvheib"   #口令 
  
  
-sender = 'jomain@ahusec.cn'
+sender = '1273046149@qq.com'
 receivers = ['1075823894@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
  
 message = MIMEText('emailcontainer', 'plain', 'utf-8')
-message['From'] = Header("jomain@ahusec.cn", 'utf-8')
+message['From'] = Header("1273046149@qq.com", 'utf-8')
 message['To'] =  Header("1075823894@qq.com", 'utf-8')
  
 subject = 'new'
